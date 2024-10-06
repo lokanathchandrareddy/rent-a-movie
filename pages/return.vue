69c2218960a4937fca16bc68a908aee40645ab21
Lokanath ChandraReddy <25945246+lokanathchandrareddy@users.noreply.github.com>
2024-10-06 17:07:12 -0400

    return page - supabase
    Based on new datastructure, update template and the fetch on mount

--- pages/return.vue
+++ pages/return.vue
@@ -1,37 +1,54 @@
 <template>
-    <VContainer>
-      <VRow>
-        <VCol
-          cols="12"
-          sm="6"
-          md="4"
-          v-for="movie in rentedMovies"
-          :key="movie.id"
-        >
-          <VCard>
-            <VCardTitle>{{ movie.title }}</VCardTitle>
-            <VCardText>
-              Quantity Rented: {{ movie.quantity }}
-            </VCardText>
-            <VCardActions>
-              <VBtn color="primary" @click="addToRentedCart(movie)">
-                Add to Return Cart
-              </VBtn>
-            </VCardActions>
-          </VCard>
-        </VCol>
+  <VContainer>
+    <VRow>
+      <VCol cols="12">
+        <h1>Your Rented Movies</h1>
+        <!-- Display rented movies list -->
+        <VList>
+          <VListItem v-for="item in rentedMovies" :key="item.id">
+              <VListItemTitle>{{ item.title }}</VListItemTitle>
+              <VListItemSubtitle>
+                Rented on: {{ formatDate(item.rented_on) }}<br />
+                Price: ${{ item.price }}<br />
+                Stock: {{ item.stock }}
+              </VListItemSubtitle>
+            <!-- Return Button -->
+            <VBtn size='x-small' color="primary" @click="addToRentedCart(item)">
+              Add to Return Cart
+            </VBtn>
+          </VListItem>
+        </VList>
+      </VCol>
     </VRow>
-    </VContainer>
+  </VContainer>
 </template>
-<script setup>
-import { computed } from 'vue';
-import { useMovieStore} from '@/stores/movieCart';
 
+<script setup>
+import { computed, onMounted } from 'vue';
+import { useMovieStore } from '~/stores/movieCart';
+
+
+// Initialize the cart store and user
+const user = useSupabaseUser();
 const movieCartStore = useMovieStore();
 
+// Fetch rented movies when component mounts
+onMounted(async () => {
+  if (user.value) {
+    await movieCartStore.fetchRentedMovies(user.value.id); // Fetch rented movies for the user
+  }
+});
+
+// Computed property to get the rented movies
 const rentedMovies = computed(() => movieCartStore.rentedItems);
 
+// Function to format the rented_on date
+const formatDate = (dateString) => {
+  return new Date(dateString).toLocaleDateString();
+};
+
+// Function to add a movie to the return cart
 function addToRentedCart(movie) {
-    movieCartStore.addToCart(movie);
+  movieCartStore.addToCart(movie);
 }
 </script>
